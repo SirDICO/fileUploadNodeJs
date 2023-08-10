07f88634f8f8206b09de40469e6c85489596d1e2
Sir DICO <88980314+SirDICO@users.noreply.github.com>
2023-08-10 10:59:42 +0100

    initial update

---
+++ custom-ndiulo-api.js
@@ -0,0 +1,169 @@
+[
+    {
+        "shipping": false,
+        "_id": "64d4ab61c78b2821f050b4d2",
+        "name": "Rugged Material",
+        "price": 30000,
+        "image": "https://res.cloudinary.com/dxismgcte/image/upload/v1691658874/file-upload/tmp-6-1691658872608_ztkwu7.jpg",
+        "colors": "#ff000",
+        "company": "ndiulo",
+        "description": "best rugg material in the Market",
+        "category": "Floor Mat",
+        "__v": 0
+    },
+    {
+        "shipping": false,
+        "_id": "64d4abf7c78b2821f050b4d4",
+        "name": "Floor Material",
+        "price": 80000,
+        "image": "https://res.cloudinary.com/dxismgcte/image/upload/v1691658862/file-upload/tmp-5-1691658861142_kapc4x.jpg",
+        "colors": "#00ff00",
+        "company": "ndiulo",
+        "description": " best rugg material in the Marketbest rugg material in the Marketbest rugg material in the Market",
+        "category": "Palour Mat",
+        "__v": 0
+    },
+    {
+        "[
+        {
+            "shipping": false,
+            "_id": "64614f65079eaf02dcb8bf0b",
+            "name": "Product name",
+            "price": 30000,
+            "image": "/uploads/computer-2.jpeg",
+            "createdAt": "2023-05-14T21:15:17.883Z",
+            "updatedAt": "2023-05-14T21:15:17.883Z",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "64614fd5079eaf02dcb8bf0d",
+            "name": "Product name",
+            "price": 30000,
+            "image": "/uploads/computer-2.jpeg",
+            "createdAt": "2023-05-14T21:17:09.423Z",
+            "updatedAt": "2023-05-14T21:17:09.423Z",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "646151de74c02b3a00a2817e",
+            "name": "Product 2",
+            "price": 30000,
+            "image": "/uploads/computer-2.jpeg",
+            "createdAt": "2023-05-14T21:25:50.227Z",
+            "updatedAt": "2023-05-14T21:25:50.227Z",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "6461524274c02b3a00a28180",
+            "name": "Product 1",
+            "price": 30000,
+            "image": "/uploads/computer-1.jpeg",
+            "createdAt": "2023-05-14T21:27:30.534Z",
+            "updatedAt": "2023-05-14T21:27:30.534Z",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "6461552f0e01ce4bd88511f3",
+            "name": "computer",
+            "price": 150.99,
+            "image": "/uploads/computer-4.jpeg",
+            "createdAt": "2023-05-14T21:39:59.035Z",
+            "updatedAt": "2023-05-14T21:39:59.035Z",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "646155300e01ce4bd88511f6",
+            "name": "computer",
+            "price": 150.99,
+            "image": "/uploads/computer-4.jpeg",
+            "createdAt": "2023-05-14T21:40:00.530Z",
+            "updatedAt": "2023-05-14T21:40:00.530Z",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "646155320e01ce4bd88511f9",
+            "name": "computer",
+            "price": 150.99,
+            "image": "/uploads/computer-4.jpeg",
+            "createdAt": "2023-05-14T21:40:02.091Z",
+            "updatedAt": "2023-05-14T21:40:02.091Z",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "64d4ab61c78b2821f050b4d2",
+            "name": "Rugged Material",
+            "price": 30000,
+            "image": "https://res.cloudinary.com/dxismgcte/image/upload/v1691658874/file-upload/tmp-6-1691658872608_ztkwu7.jpg",
+            "colors": "#ff000",
+            "company": "ndiulo",
+            "description": "best rugg material in the Market",
+            "category": "Floor Mat",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "64d4abf7c78b2821f050b4d4",
+            "name": "Floor Material",
+            "price": 80000,
+            "image": "https://res.cloudinary.com/dxismgcte/image/upload/v1691658862/file-upload/tmp-5-1691658861142_kapc4x.jpg",
+            "colors": "#00ff00",
+            "company": "ndiulo",
+            "description": " best rugg material in the Marketbest rugg material in the Marketbest rugg material in the Market",
+            "category": "Palour Mat",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "64d4ac28c78b2821f050b4d6",
+            "name": "Kitchen Material",
+            "price": 80000,
+            "image": "https://res.cloudinary.com/dxismgcte/image/upload/v1691658852/file-upload/tmp-4-1691658850346_l05z0b.jpg",
+            "colors": "#00ff00",
+            "company": "ndiulo",
+            "description": " best rugg material in the Marketbest rugg material in the Marketbest rugg material in the Market",
+            "category": "Floor Mat",
+            "__v": 0
+        },
+        {
+            "shipping": false,
+            "_id": "64d4ac57c78b2821f050b4d8",
+            "name": "Kitchen Material",
+            "price": 80000,
+            "image": "https://res.cloudinary.com/dxismgcte/image/upload/v1691658798/file-upload/tmp-2-1691658797366_jc96nq.jpg",
+            "colors": "#00ff00",
+            "company": "ndiulo",
+            "description": " best rugg material in the Marketbest rugg material in the Marketbest rugg material in the Market",
+            "category": "Floor Mat",
+            "__v": 0
+        }
+    ]shipping": false,
+        "_id": "64d4ac28c78b2821f050b4d6",
+        "name": "Kitchen Material",
+        "price": 80000,
+        "image": "https://res.cloudinary.com/dxismgcte/image/upload/v1691658852/file-upload/tmp-4-1691658850346_l05z0b.jpg",
+        "colors": "#00ff00",
+        "company": "ndiulo",
+        "description": " best rugg material in the Marketbest rugg material in the Marketbest rugg material in the Market",
+        "category": "Floor Mat",
+        "__v": 0
+    },
+    {
+        "shipping": false,
+        "_id": "64d4ac57c78b2821f050b4d8",
+        "name": "Kitchen Material",
+        "price": 80000,
+        "image": "https://res.cloudinary.com/dxismgcte/image/upload/v1691658798/file-upload/tmp-2-1691658797366_jc96nq.jpg",
+        "colors": "#00ff00",
+        "company": "ndiulo",
+        "description": " best rugg material in the Marketbest rugg material in the Marketbest rugg material in the Market",
+        "category": "Floor Mat",
+        "__v": 0
+    }
+]
--- models/Product.js
+++ models/Product.js
@@ -9,10 +9,36 @@ const ProductSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  
+  colors:{
+    type:String,
+    enum:{
+       values:['#ff000', '#00ff00', '#0000ff'],
+        message:'{VALUE} is not supported',
+      }
+  },
+   
   image: {
     type: String,
     required: true,
   },
+  company:{
+    type:String,
+    required:true,
+  },
+  description:{
+    type:String,
+    required:true,
+  },
+  category:{
+    type: String,
+    required:true,
+  },
+  shipping:{
+    type:Boolean,
+    default:false
+  },
+  
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
