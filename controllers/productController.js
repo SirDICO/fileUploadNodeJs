be7fcd8c1b32bab10e96409499dc881df8602ee8
Sir DICO <88980314+SirDICO@users.noreply.github.com>
2023-08-10 11:29:09 +0100

    updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,8 +9,20 @@ const getAllProducts = async (req, res) => {
   const products = await Product.find({});
   res.status(StatusCodes.OK).json({ products });
 };
+const getSingleProduct = async (req, res) => {
+
+  const {id: productID} = req.params
+  const product = await Product.findOne({_id:productID})
+ 
+  if(!product){
+     return next(createCustomError(`No Product with id : ${product}`, 404));
+  
+  }
+  res.status(StatusCodes.OK).json({ product });
+};
 
 module.exports = {
   createProduct,
   getAllProducts,
+  getSingleProduct,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {
   createProduct,
   getAllProducts,
+  getSingleProduct,
 } = require('../controllers/productController');
 const { uploadProductImage } = require('../controllers/uploadsController');
 
 router.route('/').post(createProduct).get(getAllProducts);
+router.route('/:id').get(getSingleProduct);
 router.route('/uploads').post(uploadProductImage);
 
 module.exports = router;
